6757e17571a6d28754d8ed13daa4c49747c4a1e8
Twyxt95 <a.schmidt0101@web.de>
2015-11-30 21:58:42 +0100

    Bilder einladen+Testbild-Pfad
    Audio in Unthief vorerst auskommentiert

--- src/control/MainControl.java
+++ src/control/MainControl.java
@@ -1,8 +1,14 @@
 package control;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Observable;
 import java.util.Observer;
+
+import javax.imageio.ImageIO;
 
 import data.DeNoise;
 import data.Filter;
@@ -10,12 +16,14 @@ import data.Images;
 import data.Interference;
 import data.Noise;
 import data.User;
+import javafx.scene.image.Image;
 
 public class MainControl extends Observable{
 	private User user;
 	private ArrayList<Interference> interference = new ArrayList<>();
 	private ArrayList<Filter> filter = new ArrayList<>();
-	private Images images;
+	private ArrayList<BufferedImage> images = new ArrayList<>();
+	//private Images images;
 	/** true solange das Spiel laeuft. */
 	private boolean isRunning = true;
 
@@ -73,11 +81,23 @@ public class MainControl extends Observable{
 	}
 	
 	public void back(){
-		
+		if(images.size()>=2){
+			images.remove(images.size()-1);
+		}
 	}
 	
 	public void randomImage(){
-		
+		try {
+			loadImage();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void loadImage() throws IOException{
+		BufferedImage image = ImageIO.read(new FileInputStream("res/pix/testbild.jpg"));
+		images.add(image);
 	}
 	
 	public void randomCode(){
